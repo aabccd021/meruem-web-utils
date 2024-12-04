d3b435e5e4f0c29b66daf1e1cefddd82ceb981b6
aabccd021 <aabccd021@gmail.com>
2024-12-04 08:49:19 +0700

    feat: Add back button functionality using MutationObserver, observe body for childList and subtree. Add tsconfig.json.

--- back-button.ts
+++ back-button.ts
@@ -1,5 +1,13 @@
-document.querySelectorAll("[data-back-button]").forEach((button) => {
-  button.addEventListener("click", () => {
-    history.back();
+new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      if (node instanceof HTMLElement) {
+        node.querySelectorAll("[data-back-button]").forEach((button) => {
+          button.addEventListener("click", () => {
+            history.back();
+          });
+        });
+      }
+    });
   });
-});
+}).observe(document.body, { childList: true, subtree: true });
