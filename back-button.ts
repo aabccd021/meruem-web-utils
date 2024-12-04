552eb6a2e281fb13be480e5330a6f537bdf8c06f
aabccd021 <aabccd021@gmail.com>
2024-12-04 09:35:48 +0700

    Refactor back-button.ts and implement dynamic event binding.

--- back-button.ts
+++ back-button.ts
@@ -1,19 +1,27 @@
-function bindEvents(parent: HTMLElement) {
+function bindEventDynamic(parent: HTMLElement, binder: (parent: HTMLElement) => void) {
+  binder(parent);
+  new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      mutation.addedNodes.forEach((node) => {
+        if (node instanceof HTMLElement) {
+          // remove all previous event listeners
+          const newNode = node.cloneNode(true) as HTMLElement;
+          node.parentNode?.replaceChild(newNode, node);
+          binder(node);
+        }
+      });
+    });
+  }).observe(parent, { childList: true, subtree: true });
+}
+
+bindEventDynamic(document.body, (parent) => {
   parent.querySelectorAll("[data-back-button]").forEach((button) => {
+    if (history.length === 1) {
+      button.setAttribute("disabled", "");
+      return;
+    }
     button.addEventListener("click", () => {
       history.back();
     });
   });
-}
-
-new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    mutation.addedNodes.forEach((node) => {
-      if (node instanceof HTMLElement) {
-        bindEvents(node);
-      }
-    });
-  });
-}).observe(document.body, { childList: true, subtree: true });
-
-bindEvents(document.body);
+})
